ec8c4f529071a0baa03668f72c666720f8369d7d
josephstorey <jsto890@github.com>
2023-03-24 12:15:11 +1300

    new test script changes to see printed matrix and vector

--- test_lusolver.py
+++ test_lusolver.py
@@ -12,8 +12,14 @@ class TestLUSolver(unittest.TestCase):
 
         # check that matrix A was read correctly
         expected_a = np.array([[2, -1, 3], [-8, 3, -8], [-2, -2, 7]])
-        np.testing.assert_array_equal(solver.matrix_a, expected_a)
+        test1 = solver.matrix_a == expected_a
+        print(solver.matrix_a)
+        print(expected_a)
+        print(test1)
 
         # check that vector b was read correctly
         expected_b = np.array([-5, 20, 3])
-        np.testing.assert_array_equal(solver.vector_b, expected_b)
+        test2 = solver.vector_b == expected_b
+        print(solver.vector_b)
+        print(expected_b)
+        print(test2)
