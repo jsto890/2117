b15d0d843c2f7f3b6c008d3d215df84af54742f0
afos743 <afos743@aucklanduni.ac.nz>
2023-03-29 09:49:53 +1300

    Wrote the 100 solution file

---
+++ 100_solution_file.py
@@ -0,0 +1,12 @@
+# Solving all 100 problems
+
+from module_lab2_task1 import *
+
+for i in range(100):
+    Solution = LUSolver()
+    Solution.read_system_from_file(f"./problems/problem{i}.txt")
+    Solution.lu_factors()
+    Solution.forward_sub()
+    Solution.backward_sub()
+    Solution.write_solution_to_file(f"./problems/solution{i}.txt")
+
