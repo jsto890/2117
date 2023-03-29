7a943cbbc03842fcef4f23b3990c67ffa8fe4e5a
afos743 <afos743@aucklanduni.ac.nz>
2023-03-29 14:19:45 +1300

    Adding the commit_history.txt

--- 100_solution_file.py
+++ 100_solution_file.py
@@ -2,7 +2,7 @@
 
 from module_lab2_task1 import *
 
-for i in range(100):
+for i in range(101):
     Solution = LUSolver()
     Solution.read_system_from_file(f"./problems/problem{i}.txt")
     Solution.lu_factors()
